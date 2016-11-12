da7ed4ed66730b6903741153f33f7a8b67a1e169
Volodymyr Shatskyi <shockone89@gmail.com>
2016-11-12 22:56:42 +0200

    Add structure.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,32 @@
 module Main where
 
 import Lib
+import Control.Monad.Free
+
+data Op next
+  = Exec String next
+  | End
+  deriving (Functor)
+
+
+type Config = Free Op
+
+exec :: String -> Config ()
+exec x = liftF $ Exec x ()
+
+end :: Config a
+end = liftF End
+
+
+config :: Config a
+config = do
+  exec "google-chrome-unstable"
+  end
+
+interpret :: Config a -> String
+interpret = interpret' ""
+  where interpret' accumulator (Free (Exec x next)) = interpret' (accumulator ++ "exec " ++ x ++ "\n") next
+        interpret' accumulator (Free End) = accumulator
 
 main :: IO ()
-main = someFunc
+main = putStrLn $ interpret config
