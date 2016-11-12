fdbf1e7fa5dd3f716d50e71d7d83dbd4864405d9
Volodymyr Shatskyi <shockone89@gmail.com>
2016-11-12 23:02:29 +0200

    Get rid of End.

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,6 @@ import Control.Monad.Free
 
 data Op next
   = Exec String next
-  | End
   deriving (Functor)
 
 
@@ -14,19 +13,15 @@ type Config = Free Op
 exec :: String -> Config ()
 exec x = liftF $ Exec x ()
 
-end :: Config a
-end = liftF End
-
-
-config :: Config a
+config :: Config ()
 config = do
   exec "google-chrome-unstable"
-  end
+  return ()
 
 interpret :: Config a -> String
 interpret = interpret' ""
   where interpret' accumulator (Free (Exec x next)) = interpret' (accumulator ++ "exec " ++ x ++ "\n") next
-        interpret' accumulator (Free End) = accumulator
+        interpret' accumulator (Pure _) = accumulator
 
 main :: IO ()
 main = putStrLn $ interpret config
