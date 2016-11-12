9ceb7838986835d9f38f9e30127e1b8bf9d87944
Volodymyr Shatskyi <shockone89@gmail.com>
2016-11-12 23:10:33 +0200

    Add exec_always.

--- app/Main.hs
+++ app/Main.hs
@@ -5,22 +5,33 @@ import Control.Monad.Free
 
 data Op next
   = Exec String next
+  | ExecAlways String next
   deriving (Functor)
-
 
 type Config = Free Op
 
 exec :: String -> Config ()
 exec x = liftF $ Exec x ()
 
+exec_always :: String -> Config ()
+exec_always x = liftF $ ExecAlways x ()
+
 config :: Config ()
 config = do
+  exec_always "xinput set-prop 12 280 0" -- Disable Tapping Drag.
+  exec_always "xinput set-prop 12 286 0.85" -- Increase Accel Speed.
+  exec_always "xmodmap ~/.xmodmap"
+
   exec "google-chrome-unstable"
+  exec "slack"
+  exec "telegram-desktop"
+
   return ()
 
 interpret :: Config a -> String
 interpret = interpret' ""
   where interpret' accumulator (Free (Exec x next)) = interpret' (accumulator ++ "exec " ++ x ++ "\n") next
+        interpret' accumulator (Free (ExecAlways x next)) = interpret' (accumulator ++ "exec_always " ++ x ++ "\n") next
         interpret' accumulator (Pure _) = accumulator
 
 main :: IO ()
