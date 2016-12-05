c0841cc4d2f38379f37e5920f54c67e38cf6e224
Volodymyr Shatskyi <shockone89@gmail.com>
2016-12-05 13:54:32 +0200

    Rename lh to hoist.

--- app/DSL.hs
+++ app/DSL.hs
@@ -5,143 +5,143 @@ import Languages.I3
 import DataTypes.Other
 import Control.Monad.Free
 
-class LH f g where
-  lh :: (() -> f ()) -> Free g ()
+class Hoist f g where
+  hoist :: (() -> f ()) -> Free g ()
 
-instance LH TopLevelF TopLevelF where
-  lh fx = liftF $ fx ()
+instance Hoist TopLevelF TopLevelF where
+  hoist fx = liftF $ fx ()
 
-instance LH BindingF BindingF where
-  lh fx = liftF $ fx ()
+instance Hoist BindingF BindingF where
+  hoist fx = liftF $ fx ()
 
-instance LH ActionF ActionF where
-  lh fx = liftF $ fx ()
+instance Hoist ActionF ActionF where
+  hoist fx = liftF $ fx ()
 
-instance LH StatementF TopLevelF where
-  lh fx = liftF $ LL (fx ())
+instance Hoist StatementF TopLevelF where
+  hoist fx = liftF $ LL (fx ())
 
-instance LH BindingF TopLevelF where
-  lh fx = liftF $ RR (fx ())
+instance Hoist BindingF TopLevelF where
+  hoist fx = liftF $ RR (fx ())
 
 exec :: String -> Free ActionF ()
-exec x = lh $ ActionF $ Exec x
+exec x = hoist $ ActionF $ Exec x
 
 moveToWorkspace :: WorkspaceNumber -> Free ActionF ()
-moveToWorkspace x = lh $ ActionF $ MoveToWorkspace x
+moveToWorkspace x = hoist $ ActionF $ MoveToWorkspace x
 
 focusWorkspace :: WorkspaceNumber -> Free ActionF ()
-focusWorkspace x = lh $ ActionF $ FocusWorkspace x
+focusWorkspace x = hoist $ ActionF $ FocusWorkspace x
 
 moveToPosition :: Int -> Int -> Free ActionF ()
-moveToPosition x y = lh $ ActionF $ MoveToPosition x y
+moveToPosition x y = hoist $ ActionF $ MoveToPosition x y
 
 resizeTo :: Int -> Int -> Free ActionF ()
-resizeTo x y = lh $ ActionF $ ResizeTo x y
+resizeTo x y = hoist $ ActionF $ ResizeTo x y
 
 activateMode :: ModeIdentifier -> Free ActionF ()
-activateMode id = lh $ ActionF $ ActivateMode id
+activateMode id = hoist $ ActionF $ ActivateMode id
 
 moveLeft :: Int -> Free ActionF ()
-moveLeft x = lh $ ActionF $ MoveLeft x
+moveLeft x = hoist $ ActionF $ MoveLeft x
 
 moveRight :: Int -> Free ActionF ()
-moveRight x = lh $ ActionF $ MoveRight x
+moveRight x = hoist $ ActionF $ MoveRight x
 
 moveDown :: Int -> Free ActionF ()
-moveDown x = lh $ ActionF $ MoveDown x
+moveDown x = hoist $ ActionF $ MoveDown x
 
 moveUp :: Int -> Free ActionF ()
-moveUp x = lh $ ActionF $ MoveUp x
+moveUp x = hoist $ ActionF $ MoveUp x
 
 moveToCenter :: Free ActionF ()
-moveToCenter = lh $ ActionF MoveToCenter
+moveToCenter = hoist $ ActionF MoveToCenter
 
 layoutStacking :: Free ActionF ()
-layoutStacking = lh $ ActionF LayoutStacking
+layoutStacking = hoist $ ActionF LayoutStacking
 
 layoutTabbed :: Free ActionF ()
-layoutTabbed = lh $ ActionF LayoutTabbed
+layoutTabbed = hoist $ ActionF LayoutTabbed
 
 layoutSplitHorizontally :: Free ActionF ()
-layoutSplitHorizontally = lh $ ActionF LayoutSplitHorizontally
+layoutSplitHorizontally = hoist $ ActionF LayoutSplitHorizontally
 
 layoutSplitVertically :: Free ActionF ()
-layoutSplitVertically = lh $ ActionF LayoutSplitVertically
+layoutSplitVertically = hoist $ ActionF LayoutSplitVertically
 
 reloadWM :: Free ActionF ()
-reloadWM = lh $ ActionF ReloadWM
+reloadWM = hoist $ ActionF ReloadWM
 
 restartWM :: Free ActionF ()
-restartWM = lh $ ActionF RestartWM
+restartWM = hoist $ ActionF RestartWM
 
 exitWM :: Free ActionF ()
-exitWM = lh $ ActionF ExitWM
+exitWM = hoist $ ActionF ExitWM
 
 focusLeft :: Free ActionF ()
-focusLeft = lh $ ActionF FocusLeft
+focusLeft = hoist $ ActionF FocusLeft
 
 focusRight :: Free ActionF ()
-focusRight = lh $ ActionF FocusRight
+focusRight = hoist $ ActionF FocusRight
 
 moveToScratchpad :: Free ActionF ()
-moveToScratchpad = lh $ ActionF MoveToScratchpad
+moveToScratchpad = hoist $ ActionF MoveToScratchpad
 
 stickyEnable :: Free ActionF ()
-stickyEnable = lh $ ActionF StickyEnable
+stickyEnable = hoist $ ActionF StickyEnable
 
 floatingEnable :: Free ActionF ()
-floatingEnable = lh $ ActionF FloatingEnable
+floatingEnable = hoist $ ActionF FloatingEnable
 
 fullscreenEnable :: Free ActionF ()
-fullscreenEnable = lh $ ActionF FullscreenEnable
+fullscreenEnable = hoist $ ActionF FullscreenEnable
 
 fullscreenToggle :: Free ActionF ()
-fullscreenToggle = lh $ ActionF FullscreenToggle
+fullscreenToggle = hoist $ ActionF FullscreenToggle
 
 splitToggle :: Free ActionF ()
-splitToggle = lh $ ActionF SplitToggle
+splitToggle = hoist $ ActionF SplitToggle
 
 focusModeToggle :: Free ActionF ()
-focusModeToggle = lh $ ActionF FocusModeToggle
+focusModeToggle = hoist $ ActionF FocusModeToggle
 
 floatingToggle :: Free ActionF ()
-floatingToggle = lh $ ActionF FloatingToggle
+floatingToggle = hoist $ ActionF FloatingToggle
 
 toggleScratchpad :: Free ActionF ()
-toggleScratchpad = lh $ ActionF ToggleScratchpad
+toggleScratchpad = hoist $ ActionF ToggleScratchpad
 
 closeWindow :: Free ActionF ()
-closeWindow = lh $ ActionF CloseWindow
+closeWindow = hoist $ ActionF CloseWindow
 
 focusFloating :: Free ActionF ()
-focusFloating = lh $ ActionF FocusFloating
+focusFloating = hoist $ ActionF FocusFloating
 
 focusTiling :: Free ActionF ()
-focusTiling = lh $ ActionF FocusTiling
+focusTiling = hoist $ ActionF FocusTiling
 
 resize :: GrowOrShrink -> WidthOrHeight -> Int -> Free ActionF ()
-resize gs wh x = lh $ ActionF $ Resize gs wh x
+resize gs wh x = hoist $ ActionF $ Resize gs wh x
 
 exit :: Free ActionF ()
-exit = lh $ ActionF $ ActivateMode (ModeIdentifier "default")
+exit = hoist $ ActionF $ ActivateMode (ModeIdentifier "default")
 
-exec' x = lh $ ExecStatement x
-execAlways a = lh $ ExecAlways a
-raw a = lh $ Raw a
-font a b = lh $ Font a b
+exec' x = hoist $ ExecStatement x
+execAlways a = hoist $ ExecAlways a
+raw a = hoist $ Raw a
+font a b = hoist $ Font a b
 
-s ==> a = lh $ BindingF $ bind s a
+s ==> a = hoist $ BindingF $ bind s a
 
 -- Bind and exit mode.
 s ==>^ a = s ==> ActionsWithCriteria criteria (actions >> exit)
   where (ActionsWithCriteria criteria actions) = addCriteria a
 
-bar x = lh $ Bar x
-hideEdgeBorders _ = lh HideEdgeBorders
-forWindow criteria actions = lh $ ForWindow (ActionsWithCriteria criteria actions)
+bar x = hoist $ Bar x
+hideEdgeBorders _ = hoist HideEdgeBorders
+forWindow criteria actions = hoist $ ForWindow (ActionsWithCriteria criteria actions)
 
 mode name config = do
-  lh $ ModeDefinition identifier bindings
+  hoist $ ModeDefinition identifier bindings
   return identifier
   where identifier = ModeIdentifier name
         bindings = do
